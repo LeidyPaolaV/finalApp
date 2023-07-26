7b619398134eff2c27f8305e437ff2fd179ba2f3
DaniLeon <122113337+Danileon0226@users.noreply.github.com>
2023-07-25 20:39:46 -0500

    ok

---
+++ app/src/main/java/com/example/turismofinal/TurismoHome.java
@@ -0,0 +1,14 @@
+package com.example.turismofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TurismoHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turismo_home);
+    }
+}
